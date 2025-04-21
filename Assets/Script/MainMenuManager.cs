0204c9a56f1b7c3ceac7b07a594b3aadb0b33d96
ObedientPutro <obedientjunior@gmail.com>
2025-04-21 20:15:55 +0800

    feat: main menu, scan menu and about menu

---
+++ Assets/Script/MainMenuManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject scanMenu;
+    [SerializeField] private GameObject settingsMenu;
+    [SerializeField] private GameObject aboutMenu;
+    [SerializeField] private GameObject tutorialMenu;
+
+    private void Start() {
+        ShowMainMenu();
+    }
+
+    private void SetAllMenusInactive() {
+        mainMenu.SetActive(false);
+        scanMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        aboutMenu.SetActive(false);
+        tutorialMenu.SetActive(false);
+    }
+
+    public void ShowMainMenu() {
+        SetAllMenusInactive();
+        mainMenu.SetActive(true);
+    }
+
+    public void ShowScanMenu() {
+        SetAllMenusInactive();
+        scanMenu.SetActive(true);
+    }
+
+    public void ShowSettingsMenu() {
+        SetAllMenusInactive();
+        settingsMenu.SetActive(true);
+    }
+
+    public void ShowAboutMenu() {
+        SetAllMenusInactive();
+        aboutMenu.SetActive(true);
+    }
+
+    public void ShowTutorialMenu() {
+        SetAllMenusInactive();
+        tutorialMenu.SetActive(true);
+    }
+}
